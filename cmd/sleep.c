09beb6069ef5ab1d6dd9596cfc3d7547b2e7ddf4
roddo <roddo@gmail.com>
2025-08-01 13:18:45 -0400

    fix version pushed

---
+++ cmd/sleep.c
@@ -0,0 +1,26 @@
+#include "../include/user.h"
+
+int
+main(argc, argv)
+int argc;
+char **argv;
+{
+	int c, n;
+	char *s;
+
+	n = 0;
+	if(argc < 2) {
+		printf("arg count\n");
+		exit(0);
+	}
+	s = argv[1];
+	while((c = *s++)) {
+		if(c<'0' || c>'9') {
+			printf("bad character\n");
+			exit(0);
+		}
+		n = n*10 + c - '0';
+	}
+	usleep(n * 1000000);
+}
+
