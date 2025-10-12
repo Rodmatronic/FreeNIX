9d0572ddfef9d5465063d8ea081991483343931c
SuperPou <superpou1filip@gmail.com>
2025-10-12 17:01:22 +0200

    add -u and -U to ps

--- cmd/ps.c
+++ cmd/ps.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <types.h>
 #include <pwd.h>
+#include <errno.h>
 
 int jflg;
-int errflg;
 int aflg;
+int uflg;
+int uargi;
+char uargstr[128];
+char usrcmp[128];
+int errflg;
 
 usage()		/* print usage message and quit */
 {
-	static char usage[] = "ps [ -aj ]";
+	static char usage[] = "ps [ -aj ] [ -u uid ] [ -U username ]";
 
 	fprintf(stderr, "usage: %s\n", usage);
 	exit(1);
@@ -27,12 +32,20 @@ int printprocs() {
 	for (int i = 0; i < pamount; i++) {
 
 		if (!getproc(i, &proc)) {
+			pw = getpwuid(proc.p_uid);
 			if (!aflg) {
 				if (proc.p_uid == getuid()) isusr = 1;
 				else isusr = 0;
 			}
+			if (uflg == 1){
+				if (uargi == proc.p_uid) isusr = 1;
+				else isusr = 0;
+			} else if (uflg == 2 && pw) {
+				strcpy(&usrcmp, pw->pw_name);
+				if (!strcmp(uargstr, usrcmp)) isusr = 1;
+				else isusr = 0;
+			}
 			if (isusr) {
-				pw = getpwuid(proc.p_uid);
 				printf("%7d ", proc.p_pid);
 				if (jflg) printf("%6d ", proc.p_gid);
 				if (pw) printf("%-8s ", pw->pw_name);
@@ -48,13 +61,21 @@ main (int argc, char *argv[]) {
 	char c;
 	int pgerrflg = 0;
 
-	while ((c = getopt(argc, argv, "ja")) != EOF) {
+	while ((c = getopt(argc, argv, "jau:U:")) != EOF) {
 		switch (c) {
 			case 'j':
 				jflg++;
 				break;
 			case 'a':
 				aflg++;
+				break;
+			case 'u':
+				uflg++;
+				uargi = atoi(optarg);
+				break;
+			case 'U':
+				uflg = 2;
+				strcpy(&uargstr, optarg);
 				break;
 			default:
 				errflg++;
